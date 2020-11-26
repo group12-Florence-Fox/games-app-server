70f6917bf32fc6f7a8518d8dc37c30edab399168
zaidanammar <zaidanammar23@gmail.com>
2020-11-26 21:47:14 +0700

    Add User Server: 1. Add feature register 2. Add feature Login 3. Add feature authentication

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,9 +1,46 @@
 const {User} = require('../models')
+const  {comparePassword} = require('../helper/bcrypt')
+const {generateToken} = require('../helper/jwt')
 
 class Controller {
-    static async register(req, res) {}
+    static async register(req, res) {
+        try {
+            let newUser = {
+                email: req.body.email,
+                password: req.body.password
+            }
+            const data = await User.create(newUser)
+            res.status(201).json({id: data.id, email: data.email})
+        } catch (error) {
+            res.status(400).json({msg: "Email telah digunakan!"})
+        }
 
-    static async login(req, res) {}
+    }
+
+    static async login(req, res) {
+        try {
+            const data = await User.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            if (!data) res.status(400).json({msg: "Invalid email or password!"})
+            else if(comparePassword(req.body.password, data.password)) {
+               
+                const access_token =  generateToken({id: data.id, email: data.email})
+                res.status(200).json({access_token})
+
+            } else {
+                res.status(400).json({msg: "Invalid email or password!"})
+            }
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    }
+
+    static async getGames(req, res) {
+        res.status(200).json('yess i got it')
+    }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const Controller = require('../controllers/Controller')
+const authentication = require('../middleware/authentication')
 
 router.post('/register', Controller.register)
 router.post('/login', Controller.login)
+router.get('/games', authentication, Controller.getGames)
 
 module.exports = router
